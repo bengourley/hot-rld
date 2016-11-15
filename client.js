cc8bc40dc8b6768fdc9ad6ccc23dd72cd21080b8
Ben Gourley <bn@grly.me>
2016-11-15 15:53:22 +0000

    Set an arbitrary timeout
    Ran into the problem of the file being requested before it had
    fully been written to disk.

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 module.exports = (address = 'http://localhost:9909') => `;(function () {
   var es = new window.EventSource('${address}')
   es.onmessage = function (event) {
-    console.log('[hot-rld] ' + event.data + ' updated')
-    var script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = event.data + '?cachebust=' + Date.now()
-    var prevNode = document.querySelector('[src^="' + event.data + '"]')
-    if (!prevNode) return console.log('[hot-rld] cannot find <script> tag to replace')
-    prevNode.parentNode.replaceChild(script, prevNode)
+    setTimeout(function () {
+      console.log('[hot-rld] ' + event.data + ' updated')
+      var script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = event.data + '?cachebust=' + Date.now()
+      var prevNode = document.querySelector('[src^="' + event.data + '"]')
+      if (!prevNode) return console.log('[hot-rld] cannot find <script> tag to replace')
+      prevNode.parentNode.replaceChild(script, prevNode)
+    }, 500)
   }
 })();`
